132dadd2ef55bbac301528500e8a4538caf4b163
Petr Vaněk <petr.vanek@e2b.dev>
2026-09-26 16:32:55 +0200

    refactor(shared): remove unused ParseBodyWith
    GitOrigin-RevId: 3c8a46f7cc3b730c6fa4c849282423492d862b15

--- packages/shared/pkg/ginutils/body.go
+++ packages/shared/pkg/ginutils/body.go
@@ -46,14 +46,3 @@ func ParseOptionalBody[B any](ctx context.Context, c *gin.Context) (body B, err 
 
 	return body, nil
 }
-
-func ParseBodyWith[B any](ctx context.Context, c *gin.Context, parse func(io.Reader) (B, error)) (body B, err error) {
-	body, err = parse(c.Request.Body)
-	if err != nil {
-		telemetry.ReportCriticalError(ctx, "error when parsing request", err)
-
-		return body, fmt.Errorf("error when parsing request: %w", err)
-	}
-
-	return body, nil
-}
